9ba58fec204c13dae3bdcb1e56598f73c3d850e8
unknown <carlmoore256@gmail.com>
2023-05-02 02:51:16 -0700

    tweaking setup.sh to download model correctly

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 pip install -r requirements.txt
 mkdir data
-wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth -o data/sam_vit_h_4b8939.pth
+wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth -P data/
 git submodule init
 git submodule update
