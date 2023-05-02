a151afeceaaa25b378bb6387269515b49b56c4ef
unknown <carlmoore256@gmail.com>
2023-05-02 02:40:57 -0700

    setup.sh updates submodules

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 pip install -r requirements.txt
 mkdir data
 wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth -o data/sam_vit_h_4b8939.pth
+git submodule init
+git submodule update
