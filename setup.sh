d8a5f025d5f5aadebdf39fd57d012d6e7ecc5b64
unknown <carlmoore256@gmail.com>
2023-06-08 18:43:41 -0700

    fixed requirements bug with skimage, updated setup shell script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,17 @@
+#!/bin/bash
+
+echo "Installing dependencies..."
 pip install -r requirements.txt
+
+echo "Creating data directory..."
 mkdir data
+
+echo "Downloading models..."
 wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth -P data/
+
+echo "Cloning submodules..."
 git submodule init
 git submodule update
+
+echo "Cloning deepsvg..."
+git clone https://github.com/alexandre01/deepsvg.git
